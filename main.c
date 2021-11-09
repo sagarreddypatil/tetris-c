04903b55bda24e03548309c58c5d9c07dc31d816
Sagar Patil <sagarreddypatil@gmail.com>
2021-11-08 22:37:50 -0500

    incomplete broken code

--- main.c
+++ main.c
@@ -23,9 +23,9 @@ typedef struct
     Uint8 b;
 } Color;
 
-// cyan, blue, orange, yellow, green, purple, red, undefined
-const Color COLORS[8] = {{0x00, 0xff, 0xff}, {0x00, 0x00, 0xff}, {0xff, 0x7f, 0x00}, {0xff, 0xff, 0x00},
-                         {0x00, 0xff, 0x00}, {0xa2, 0x28, 0xff}, {0xff, 0x00, 0x00}, {0x00, 0x00, 0x00}};
+// undefined, cyan, blue, orange, yellow, green, purple, red
+const Color COLORS[8] = {{0x00, 0x00, 0x00}, {0x00, 0xff, 0xff}, {0x00, 0x00, 0xff}, {0xff, 0x7f, 0x00},
+                         {0xff, 0xff, 0x00}, {0x00, 0xff, 0x00}, {0xa2, 0x28, 0xff}, {0xff, 0x00, 0x00}};
 
 const SDL_Point GRID_POINTS[5] = {{GRID_START_X, GRID_START_Y},
                                   {GRID_START_X, GRID_END_Y},
@@ -33,41 +33,14 @@ const SDL_Point GRID_POINTS[5] = {{GRID_START_X, GRID_START_Y},
                                   {GRID_END_X, GRID_START_Y},
                                   {GRID_START_X, GRID_START_Y}};
 
-void drawBox(SDL_Renderer *renderer, int posX, int posY, char color)
+void drawBox(SDL_Renderer *renderer, int posX, int posY, int color)
 {
     SDL_Rect rect = {.x = posX * PIXELS_PER_GRID + GRID_START_X,
                      .y = posY * PIXELS_PER_GRID + GRID_START_Y,
                      .w = PIXELS_PER_GRID,
                      .h = PIXELS_PER_GRID};
 
-    Color boxColor;
-    switch (color)
-    {
-    case 'c':
-        boxColor = COLORS[0];
-        break;
-    case 'b':
-        boxColor = COLORS[1];
-        break;
-    case 'o':
-        boxColor = COLORS[2];
-        break;
-    case 'y':
-        boxColor = COLORS[3];
-        break;
-    case 'g':
-        boxColor = COLORS[4];
-        break;
-    case 'p':
-        boxColor = COLORS[5];
-        break;
-    case 'r':
-        boxColor = COLORS[6];
-        break;
-    default:
-        boxColor = COLORS[7];
-        break;
-    }
+    Color boxColor = COLORS[color];
     SDL_SetRenderDrawColor(renderer, boxColor.r, boxColor.g, boxColor.b, 255);
     SDL_RenderFillRect(renderer, &rect);
 }
@@ -105,22 +78,12 @@ int main()
     char gameGrid[GRID_HEIGHT][GRID_WIDTH];
     memset(gameGrid, 0, GRID_HEIGHT * GRID_WIDTH * sizeof(char));
 
-    for (int col = 0; col < GRID_WIDTH; col++)
+    for (int row = 0; row < GRID_HEIGHT; row++)
     {
-        if (col % 2 == 0)
+        for (int col = 0; col < GRID_WIDTH; col++)
         {
-            gameGrid[GRID_HEIGHT - 1][col] = 'g';
+            gameGrid[row][col] =
         }
-    }
-
-    for (int col = 0; col < GRID_WIDTH; col++)
-    {
-        gameGrid[GRID_HEIGHT - 2][col] = 'b';
-    }
-
-    for (int col = 0; col < GRID_WIDTH; col++)
-    {
-        gameGrid[0][col] = 'o';
     }
 
     bool quit = false;
